8db16145d71c4fc4c8fa7e816f6e64c9d82d1017
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2022-09-27 14:49:48 +0800

    optimize code

--- hooks/useLoadEpisodes.tsx
+++ hooks/useLoadEpisodes.tsx
@@ -1,26 +1,21 @@
 import { useEffect, useState } from "react";
-import { EpisodesQuery, useEpisodesQuery } from "../graphql/episodes.gql";
+import { useEpisodesQuery } from "../graphql/episodes.gql";
 import { Episode } from "../graphql/types";
 
 export const useLoadEpisodes = () => {
   const [items, setItems] = useState<Partial<Episode | null>[]>([]);
   const [page, setPage] = useState(1);
-  const [{ data, fetching }, fetchMore] = useEpisodesQuery({
+  const [{ data, fetching }] = useEpisodesQuery({
     variables: {
       page: page,
     },
-    pause: page > 1,
   });
 
   const episodes = data?.episodes?.results ?? [];
 
   useEffect(() => {
-    setItems([...items, ...episodes]);
+    setItems(items.concat(episodes));
   }, [episodes]);
-
-  useEffect(() => {
-    fetchMore();
-  }, [page]);
 
   const next = data?.episodes?.info?.next;
 
@@ -32,7 +27,7 @@ export const useLoadEpisodes = () => {
 
   return {
     items,
-    hasNext: !!next,
+    hasNext: Boolean(next),
     loadMore,
     fetching,
   };
